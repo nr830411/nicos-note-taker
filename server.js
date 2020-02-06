753d574b94dfc50504e7a1325cee46c0e224a3af
Nico Reed <nr830411@gmail.com>
2020-02-06 18:25:34 -0500

    (fixed PORT)

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const mainDir = path.join(__dirname, "/public");
 
 app.use(express.static('public'));
